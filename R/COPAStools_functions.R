3ea5daef6d7156add862da111ec9a98e6c36be5c
Tyler Shimko <TShimko126@gmail.com>
2014-05-28 10:23:07 -0500

    Changes to DESCRIPTION, add roxygen2 documentation to COPAStools_functions

--- R/COPAStools_functions.R
+++ R/COPAStools_functions.R
@@ -1,66 +1,81 @@
-#Extract the plate number
-plateno <- function(string)
-{
-    split<-strsplit(string,"_")
-    split<-split[[1]]
-    split<-split[1]
-    
-    split<-strsplit(split,"p")
-    split<-split[[1]]
-    plate<-split[2]
-}
+#' Read in raw sorter data
+#' 
+#' Reads a raw sorter file into a dataframe, removing NA values and any objects not fitting in to the min and max cut offs
+#' @param file path to sorter data file
+#' @param tofmin minimum cut off for time of flight, defaults to 0
+#' @param tofmax maximum cut off for time of flight, defaults to 10000
+#' @param extmin minimum cut off for extinction, defaults to 0
+#' @param extmax maximum cut off for extinction, defaults to 10000
+#' @export
+#' @examples
+#' readSorter("SortTest.txt", 60, 2000, 60, 5000)
+#' readSorter("SortTest.txt", tofmin=60, extmin=60)
 
-experimentName <- function(filePath){
-    splitfp <- strsplit(filePath,"/")
-    dirName <- splitfp[[1]][(length(splitfp[[1]]))]
-    details <- strsplit(dirName,"_")[[1]][2]
-    return(details)
-}
-
-#Extract the metadata info
-info <- function(filePath, levels = 1){
-    splitfp <- strsplit(filePath,"/")
-    dirName <- splitfp[[1]][(length(splitfp[[1]])-levels)]
-    
-    date <- strsplit(dirName,"_")[[1]][1]
-    
-    details <- strsplit(dirName,"_")[[1]][2]
-    
-    experiment <- strsplit(details,"[0-9]+")[[1]][1]
-    round <- strsplit(details,"(?i)[a-z]+")[[1]][2]
-    assay <- strsplit(details,"[0-9]+")[[1]][2]
-    
-    split <- strsplit(splitfp[[1]][(length(splitfp[[1]]))],"_")[[1]]
-    drug <- strsplit(split[2],"\\.")[[1]][1]
-    plate <- strsplit(split[1],"p")[[1]][2]
-    
-    frame <- data.frame(date,experiment,round,assay,plate,drug)
-    
-    return(frame)
-}
-
-#Function to process the setup data
-procSetup <- function(file, tofmin=60, tofmax=2000, extmin=20, extmax=5000) {
-    
-    #Read in the sorter data from the file
-    plate <- readSorter(file, tofmin, tofmax, extmin)
-    modplate <- with(plate, data.frame(row=Row, col=as.factor(Column),
-                                       sort = Status.sort, TOF=TOF, EXT=EXT,
-                                       time=Time.Stamp, green=Green, yellow=Yellow,
-                                       red=Red))
-    
-    #Process the data
-    proc <- ddply(.data=modplate[modplate$row %in% c("A","B","C","D","E","F","G", "H"),], .var=c("row", "col"), .drop=F, .fun=function(x){
-        c(pop = length(x$EXT), sorted = sum(x$sort==6), TOF = mean(x$TOF), EXT = mean(x$EXT),TOFmed=median(x$TOF),EXTmed=median(x$EXT)
-        )})
-    
-    return(proc)
+readSorter <- function(file, tofmin=0, tofmax=10000, extmin=0, extmax=10000)  {
+    data <- read.delim(file=file, header=T, na.strings=c("n/a"), as.is=T, stringsAsFactors=F)
+    data <- data[!is.na(data$TOF),]
+    data <- data[,!is.na(data[1,])]
+    data <- data[(data$TOF>=tofmin & data$TOF<=tofmax) | data$TOF == -1,]
+    data <- data[(data$EXT>=extmin & data$EXT<=extmax) | data$EXT == -1,]
+    data$Column <- as.factor(data$Column)
+    data$Row <- as.factor(data$Row)
+    return(data)
 }
 
 
+#' Set time to relative
+#' 
+#' Sets time relative to first well run, used in other functions, not meant to be used on its own
+#' @param plate a plate to extract the time from
 
-processWells <- function(modplate, strains=NULL, quantiles=FALSE, log=FALSE) {
-    processed <- suppressWarnings(ddply(.data=modplate[modplate$call50=="object" | modplate$TOF == -1,], .variables=c("row", "col"),
+extractTime <- function(plate) {plate$time <- plate$time - min(plate$time); return(plate) }
+
+
+#' Read in the sorter data with minimal processing
+#' 
+#' Returns a minimally processed data frame, optionally with SVM-mediated bubble prediction
+#' @param file path to sorter data file
+#' @param tofmin minimum cut off for time of flight, defaults to 0
+#' @param tofmax maximum cut off for time of flight, defaults to 10000
+#' @param extmin minimum cut off for extinction, defaults to 0
+#' @param extmax maximum cut off for extinction, defaults to 10000
+#' @param SVM logical dictating whether to predict bubbles with the SVM
+#' @export
+#' @examples
+#' readSorterData("SortTest.txt", 60, 2000, 60, 5000, FALSE)
+#' readSorterData("SortTest.txt", tofmin=60, extmin=60, TRUE)
+
+readSorterData <- function(file, tofmin=0, tofmax=10000, extmin=0, extmax=10000, SVM=TRUE) {
+    require(plyr)
+    plate <- readSorter(file, tofmin, tofmax, extmin, extmax)
+    modplate <- with(plate, data.frame(row=Row, col=as.factor(Column), sort=Status.sort, TOF=TOF, EXT=EXT, time=Time.Stamp, green=Green, yellow=Yellow, red=Red))
+    modplate <- ddply(.data=modplate, .variables=c("row", "col"), .fun=extractTime)
+    if(SVM){
+        require(kernlab)
+        load("~/Dropbox/Biosort/Scripts and functions/bubbleSVMmodel_noProfiler.RData")
+        plateprediction <- predict(bubbleSVMmodel_noProfiler, modplate[,3:length(modplate)], type="probabilities")
+        modplate$object <- plateprediction[,"1"]
+        modplate$call50 <- factor(as.numeric(modplate$object>0.5), levels=c(1,0), labels=c("object", "bubble"))
+    }
+    return(modplate)
+}
+
+
+#' Condense all objects examined to appropriate well
+#' 
+#' Returns a data frame with one row per well with summary statistics
+#' @param plate plate to summarize, must be run through readSorterData, not readSorter
+#' @param strains a vector of all strain (sample) names input row-wise to add to the data frame
+#' @param quantiles if TRUE, columns of trait quantiles (every fifth) will be added to the output dataframe, defaults to FALSE
+#' @param log if TRUE, columns of log transformed EXT and red fluorescence will be added to output dataframe, defaults to FALSE
+#' @export
+#' @examples
+#' processWells(plate, quantiles=TRUE)
+#' processWells(plate, quantiles=TRUE, log=TRUE)
+
+processWells <- function(plate, strains=NULL, quantiles=FALSE, log=FALSE) {
+    require(plyr)
+    processed <- suppressWarnings(ddply(.data=plate[plate$call50=="object" | plate$TOF == -1,], .variables=c("row", "col"),
                        .fun=function(x){c(n=length(x$TOF),
                                           n.sorted=sum(x$sort==6),
                                           meanTOF=mean(x$TOF),
@@ -217,95 +232,6 @@ processWells <- function(modplate, strains=NULL, quantiles=FALSE, log=FALSE) {
     return(analysis)
 }
 
-meltdf <- function(score){
-    newscore<-data.frame(row=rep(score$row,each=1),col=rep(score$col,each=1),n=rep(score$n,each=1),f.L1=rep(score$f.L1,each=1),f.L2L3=rep(score$f.L2L3,each=1),f.L4=rep(score$f.L4,each=1),f.ad=rep(score$f.ad,each=1))
-    newscore<-melt(newscore,id.var=c("row","col","n"))
-    return(newscore)
-}
-
-possContam <- function(procDataFrame){
-    strainMean <- mean(procDataFrame$n[!is.na(procDataFrame$strain)], na.rm = TRUE)
-    strainSD <- sd(procDataFrame$n[!is.na(procDataFrame$strain)], na.rm = TRUE)
-    washMean <- mean(procDataFrame$n[is.na(procDataFrame$strain)], na.rm = TRUE)
-    washSD <- sd(procDataFrame$n[is.na(procDataFrame$strain)], na.rm = TRUE)
-    possibleContam <- c()
-    for(j in seq(1,nrow(procDataFrame),)){
-        if(!is.na(procDataFrame[j,"strain"] & !is.na(procDataFrame[j,"n"]))){
-            if(procDataFrame[j,"n"] > strainMean + (2*strainSD)){
-                row <- as.character(procDataFrame[j, "row"])
-                col <- as.numeric(procDataFrame[j, "col"])
-                adjacentWash <- procDataFrame[procDataFrame$row==row & procDataFrame$col==(col+1),"n"]
-                if(adjacentWash > washMean + (2*washSD)){
-                    possibleContam <- append(possibleContam, paste0(row, col))
-                }
-            }
-        }
-    } 
-}
-
-
-#' Read in raw sorter data
-#' 
-#' Reads a raw sorter file into a dataframe, removing NA values and any objects not fitting in to the min and max cut offs
-#' @param file path to sorter data file
-#' @param tofmin minimum cut off for time of flight, defaults to 0
-#' @param tofmax maximum cut off for time of flight, defaults to 10000
-#' @param extmin minimum cut off for extinction, defaults to 0
-#' @param extmax maximum cut off for extinction, defaults to 10000
-#' @export
-#' @examples
-#' readSorter("SortTest.txt", 60, 2000, 60, 5000)
-#' readSorter("SortTest.txt", tofmin=60, extmin=60)
-
-readSorter <- function(file, tofmin=0, tofmax=10000, extmin=0, extmax=10000)  {
-    data <- read.delim(file=file, header=T, na.strings=c("n/a"), as.is=T, stringsAsFactors=F)
-    data <- data[!is.na(data$TOF),]
-    data <- data[,!is.na(data[1,])]
-    data <- data[(data$TOF>=tofmin & data$TOF<=tofmax) | data$TOF == -1,]
-    data <- data[(data$EXT>=extmin & data$EXT<=extmax) | data$EXT == -1,]
-    data$Column <- as.factor(data$Column)
-    data$Row <- as.factor(data$Row)
-    return(data)
-}
-
-
-#' Set time to relative
-#' 
-#' Sets time relative to first well run, used in other functions, not meant to be used on its own
-#' @param plate a plate to extract the time from
-
-extractTime <- function(plate) {plate$time <- plate$time - min(plate$time); return(plate) }
-
-
-#' Read in the sorter data with minimal processing
-#' 
-#' Returns a minimally processed data frame, optionally with SVM-mediated bubble prediction
-#' @param file path to sorter data file
-#' @param tofmin minimum cut off for time of flight, defaults to 0
-#' @param tofmax maximum cut off for time of flight, defaults to 10000
-#' @param extmin minimum cut off for extinction, defaults to 0
-#' @param extmax maximum cut off for extinction, defaults to 10000
-#' @param SVM logical dictating whether to predict bubbles with the SVM
-#' @export
-#' @examples
-#' readSorterData("SortTest.txt", 60, 2000, 60, 5000, FALSE)
-#' readSorterData("SortTest.txt", tofmin=60, extmin=60, TRUE)
-
-readSorterData <- function(file, tofmin=0, tofmax=10000, extmin=0, extmax=10000, SVM=TRUE) {
-    require(plyr)
-    plate <- readSorter(file, tofmin, tofmax, extmin, extmax)
-    modplate <- with(plate, data.frame(row=Row, col=as.factor(Column), sort=Status.sort, TOF=TOF, EXT=EXT, time=Time.Stamp, green=Green, yellow=Yellow, red=Red))
-    modplate <- ddply(.data=modplate, .variables=c("row", "col"), .fun=extractTime)
-    if(SVM){
-        require(kernlab)
-        load("~/Dropbox/Biosort/Scripts and functions/bubbleSVMmodel_noProfiler.RData")
-        plateprediction <- predict(bubbleSVMmodel_noProfiler, modplate[,3:length(modplate)], type="probabilities")
-        modplate$object <- plateprediction[,"1"]
-        modplate$call50 <- factor(as.numeric(modplate$object>0.5), levels=c(1,0), labels=c("object", "bubble"))
-    }
-    return(modplate)
-}
-
 
 #' Remove wells from a data frame representing a plate
 #' 
@@ -335,3 +261,20 @@ removeWells <- function(plate, badWells, drop=FALSE) {
     }
     return(plate)
 }
+
+
+
+
+
+#' 
+
+plotTrait = function(plate, trait){
+    ggplot(plate)+geom_rect(aes(xmin=0,xmax=5,ymin=0,ymax=5,fill=get(trait)))+
+        facet_grid(row~col)+geom_text(aes(x=2.5,y=2.5,label=get(trait),colour="white"))+
+        theme(axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.x=element_blank(), axis.text.y=element_blank())+
+        xlab("columns")+ylab("rows")
+}
+
+
+
+
